660241f5a022c573f2f5df5dfc02a50cc40901ee
David Fox <davidmfox123@gmail.com>
2025-01-06 18:28:24 +0000

    Update SummarySection.jsx

--- src/components/SummarySection.jsx
+++ src/components/SummarySection.jsx
@@ -3,7 +3,7 @@ import FeaturedApps from './FeaturedApps';
 
 export default function SummarySection() {
   return (
-    <section id="summary" className="bg-purple-custom text-white py-24 px-12">
+    <section id="summary" className="bg-purple-custom text-white py-24 px-3 sm:px-6">
       <div className="max-w-7xl mx-auto">
         <h3 className="text-6xl font-bold mb-12 text-center">
           Build Apps Like This <span className="text-neon-yellow">Now</span>
